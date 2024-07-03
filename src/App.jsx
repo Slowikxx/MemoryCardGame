b60ce0180476b6e7bef93e621083d4f3359e8407
Slowikxx <aniakslowik@student.agh.edu.pl>
2024-07-03 21:18:02 +0200

    - added images for cards - added and tested fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="w-full h-full flex justify-center items-center flex-col">
+			<h1 className="font-rockSalt text-4xl text-red-700">Memory Card Game</h1>
+			<h2 className="font-playWrite text-2xl text-blue-300">Start the game</h2>
+		</div>
+	);
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				rockSalt: ['Rock Salt', 'cursive'],
+				playWrite: ['Playwrite US Modern', 'cursive'],
+			},
+		},
 	},
 	plugins: [],
 };
